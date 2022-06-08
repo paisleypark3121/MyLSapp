5417b03c85fbde65d89e337a57eae487b6dfe74a
paisleypark <paisleypark@outlook.it>
2022-06-08 18:26:35 +0200

    test commit

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     //return view('welcome');
-    return "HELLO WORLD";
+    return "HELLO ALL MY WORLD";
 });
 
 Route::get('/test', function () {
