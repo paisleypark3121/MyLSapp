7052f7beb64acd2af751eb7dbe3fb7f78409d696
paisleypark <paisleypark@outlook.it>
2022-06-09 17:51:24 +0200

    new route

---
+++ app/Models/tabletest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class tabletest extends Model
+{
+    use HasFactory;
+}
